e0ea15071aeff4a2682bda2b7f321496def8f6d9
itsmonicalara <a01655306@itesm.mx>
2019-10-24 16:36:09 -0500

    Room movement w/ no errors

---
+++ Assets/Scripts/RoomMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMovement : MonoBehaviour
+{
+    public Vector2 cameraChange;
+    public Vector3 myPlayer;
+    private CameraMovement myCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCamera = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.CompareTag("Player")){
+            myCamera.minPosition += cameraChange;
+            myCamera.maxPosition += cameraChange;
+            col.transform.position += myPlayer;
+        }
+    }
+}
