2d23276db48061adad716314a9116c98dc6419c9
itsmonicalara <a01655306@itesm.mx>
2019-10-24 16:56:24 -0500

    no message

--- Assets/Scripts/RoomMovement.cs
+++ Assets/Scripts/RoomMovement.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMovement : MonoBehaviour
 {
     public Vector2 cameraChange;
     public Vector3 myPlayer;
     private CameraMovement myCamera;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +29,17 @@ public class RoomMovement : MonoBehaviour
             myCamera.minPosition += cameraChange;
             myCamera.maxPosition += cameraChange;
             col.transform.position += myPlayer;
+            if(needText){
+                StartCoroutine(PlaceNameCo());
+            }
         }
     }
+
+    private IEnumerator PlaceNameCo(){
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
+    }
+
 }
