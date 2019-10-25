73125ead9f99c9421fe9089f5408e564d3717e0a
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2019-10-25 00:31:49 -0500

    Enemie set up

---
+++ Assets/Scripts/Arbolito.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arbolito : Enemy
+{
+    public Transform myTarget;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTarget = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance(){
+        if(Vector3.Distance(myTarget.position,transform.position)<= chaseRadius &&
+         Vector3.Distance(myTarget.position,transform.position)>attackRadius){
+            transform.position = Vector3.MoveTowards(transform.position,myTarget.position,
+            moveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PotDestruction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotDestruction : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Smash(){
+        myAnimator.SetBool("smash",true);
+    }
+}
